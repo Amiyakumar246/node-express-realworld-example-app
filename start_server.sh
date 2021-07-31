3900fe1af812a29d7c1f89412358354e9e510f6f
Amiyakumar246 <66254829+Amiyakumar246@users.noreply.github.com>
2021-07-31 11:37:20 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
- #pm2 start /home/ubuntu/node-express-realworld-example-app/app.js -i 0 --name "app"
- cd /home/ubuntu/node-express-realworld-example-app
- #pm2 restart /home/ubuntu/node-express-realworld-example-app/app.js
- pm2 start /home/ubuntu/node-express-realworld-example-app/app.js -i 0 --name "app" -f
+cd /home/ubuntu/node-express-real-world-example-app
+pm2 start app.js -f
